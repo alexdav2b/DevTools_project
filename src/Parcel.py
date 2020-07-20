a7a7eed82fa13948d92a9ebb7d76bbf543f8d14e
dinesoft <amandine.bresil@free.fr>
2020-07-20 22:38:38 +0200

    Add parcels functions

---
+++ src/Parcel.py
@@ -0,0 +1,8 @@
+class Parcel:
+    def __init__(self, type):
+        self.type = type
+        # self.name
+        # self.temp
+        # self.humidity
+        # self.world_type
+        # self.food
